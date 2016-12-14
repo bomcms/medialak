aff6198aaf7d5620ecbdb44513ed5b377a7cf970
Nguyen Tuan Sieu <ideasvn@hotmail.com>
2016-12-14 20:47:06 +0700

    .

--- YoutubeAPI.php
+++ YoutubeAPI.php
@@ -4,17 +4,39 @@ namespace BomCMS\MediaLak;
 
 use GuzzleHttp\Client;
 
-class YoutubeAPI {
+class YoutubeAPI
+{
 
-    public $youtube;
-    public function __construct()
+    public $_client;
+
+    public function __construct(Client $client)
     {
-        $this->youtube = new Client([
-            'base_uri' => 'http://www.youtube.com'
-        ]);
+        $this->_client = $client;
     }
 
-    public static function getLink($id) {
-print_r($id);
+    /**
+     * title : Tiêu đề của video.
+     * thumbnail_url : Đường dẫn đến file ảnh thumbnail.
+     * author : Tên tác giả video.
+     * url_encoded_fmt_stream_map : Chứa url của các định dạng video.
+     * length_seconds : Thời lượng.
+     * @param $id
+     */
+
+    public static function getLink($id)
+    {
+        $client = new Client([
+            'base_uri' => 'http://www.youtube.com'
+        ]);
+        $data = $client->request('GET', 'get_video_info', [
+            'query' => [
+                'video_id' => $id
+            ],
+        ]);
+        $video = parse_str($data->getBody()->getContents(), $videos);
+
+
+        echo '<pre>';
+        print_r($videos);
     }
 }
